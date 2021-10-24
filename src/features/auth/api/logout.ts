a6db9c9925609816f58a6c909b4c5a36701d120d
Amine Elmouradi <elmouradi.amine98@gmail.com>
2021-10-24 13:05:43 +0200

    Feat/auth api (#1)
    * chore: prepare auth api base
    
    * chore(auth): add dto types
    
    * feat: add auth api
    
    * fix: remove path aliases

---
+++ src/features/auth/api/logout.ts
@@ -0,0 +1,8 @@
+import { auth } from 'config/firebase';
+import { signOut } from 'firebase/auth';
+
+const logout = () => {
+  return signOut(auth);
+};
+
+export default logout;
---
+++ src/features/auth/api/signup.ts
@@ -0,0 +1,9 @@
+import { createUserWithEmailAndPassword, UserCredential } from 'firebase/auth';
+import { auth } from 'config/firebase';
+import { SignupDTO } from '../types';
+
+const signup = ({ email, password }: SignupDTO): Promise<UserCredential> => {
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export default signup;
