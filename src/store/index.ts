af04468fb99940b32f8f75a84b0c392b4ab3030e
Amine Elmouradi <elmouradi.amine98@gmail.com>
2021-10-24 20:42:03 +0200

    refactor: put auth into its own store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,1 @@
-import create, { SetState } from 'zustand';
-import { Store } from 'types';
-import authSlice from './auth';
-
-const useStore = create((set: SetState<Store>) => ({
-  auth: { ...authSlice(set) },
-}));
-
-export default useStore;
+export * from './auth';
