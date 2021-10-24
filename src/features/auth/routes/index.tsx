af24cb0cf25cf8bfb553215c91d817b0ca86efe5
Amine Elmouradi <elmouradi.amine98@gmail.com>
2021-10-24 20:44:59 +0200

    refactor: update auth routes as public

--- src/features/auth/routes/index.tsx
+++ src/features/auth/routes/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { PublicRoute } from 'routes/PublicRoute';
 import { Login } from './Login';
 import { Signup } from './Signup';
 
 export const AuthRoutes = () => (
   <>
-    <Route exact path="/login" render={Login} />
-    <Route exact path="/signup" render={Signup} />
+    <PublicRoute exact path="/login" component={Login} />
+    <PublicRoute exact path="/signup" component={Signup} />
   </>
 );
